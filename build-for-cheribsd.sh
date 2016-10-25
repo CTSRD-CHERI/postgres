44f9713b626b9d240816109e5d58ac7af257c3da
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2016-10-25 23:56:38 +0000

    Add -Werror=undefined-internal
    At least the static function numeric_abbrev_convert_var is missing
    when compiling for CHERI.
    
    See issue #3

--- build-for-cheribsd.sh
+++ build-for-cheribsd.sh
@@ -5,10 +5,11 @@ export PATH=${CHERISDK}:$PATH
 export CC=${CHERISDK}/clang
 export CXX=${CHERISDK}/clang++
 READLINE_INCLUDE_DIR=${CHERIBSD_SYSROOT}/usr/include/edit/
-COMPILE_FLAGS="--sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -mabi=sandbox -msoft-float -mxgot -isystem ${READLINE_INCLUDE_DIR} -Wno-cheri-capability-misuse -Werror=implicit-function-declaration"
+COMMON_FLAGS="--sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -mabi=sandbox -msoft-float -mxgot"
+COMPILE_FLAGS="${COMMON_FLAGS} -isystem ${READLINE_INCLUDE_DIR} -Wno-cheri-capability-misuse -Werror=implicit-function-declaration -Werror=format -Werror=undefined-internal"
 export CFLAGS=${COMPILE_FLAGS}
 export CXXFLAGS=${COMPILE_FLAGS}
-export LDFLAGS="--sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -mabi=sandbox -msoft-float -mxgot"
+export LDFLAGS="${COMMON_FLAGS}"
 # env | sort
 ./configure --host=cheri-unknown-freebsd --target=cheri-unknown-freebsd --build=x86_64-unknown-freebsd --prefix=/home/alr48/cheri/postgres
 gmake -j1
