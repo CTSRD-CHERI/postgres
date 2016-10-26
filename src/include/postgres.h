5e85d744a1ae5a9c723eaae643a2078140cc5c7c
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2016-10-26 12:57:44 +0000

    Cast to uintptr_t when masking values
    Also use the PG_UINTxx_MAX constants instead of hex constants
    
    Part of issue #4

--- src/include/postgres.h
+++ src/include/postgres.h
@@ -377,22 +377,21 @@ typedef uintptr_t Datum;
 
 typedef Datum *DatumPtr;
 
-#define GET_1_BYTE(datum)	(((Datum) (datum)) & 0x000000ff)
-#define GET_2_BYTES(datum)	(((Datum) (datum)) & 0x0000ffff)
-#define GET_4_BYTES(datum)	(((Datum) (datum)) & 0xffffffff)
+#define GET_1_BYTE(datum)	(((Datum) (datum)) & (uintptr_t)PG_UINT8_MAX)
+#define GET_2_BYTES(datum)	(((Datum) (datum)) & (uintptr_t)PG_UINT16_MAX)
+#define GET_4_BYTES(datum)	(((Datum) (datum)) & (uintptr_t)PG_UINT32_MAX)
 #if SIZEOF_DATUM == 8
 #define GET_8_BYTES(datum)	((Datum) (datum))
 #elif SIZEOF_DATUM > 8
-
-#define GET_8_BYTES(datum)	(((Datum) (datum)) & 0xffffffffffffffffull)
+#define GET_8_BYTES(datum)	(((Datum) (datum)) & (uintptr_t)PG_UINT64_MAX)
 #endif
-#define SET_1_BYTE(value)	(((Datum) (value)) & 0x000000ff)
-#define SET_2_BYTES(value)	(((Datum) (value)) & 0x0000ffff)
-#define SET_4_BYTES(value)	(((Datum) (value)) & 0xffffffff)
+#define SET_1_BYTE(value)	(((Datum) (value)) & (uintptr_t)PG_UINT8_MAX)
+#define SET_2_BYTES(value)	(((Datum) (value)) & (uintptr_t)PG_UINT16_MAX)
+#define SET_4_BYTES(value)	(((Datum) (value)) & (uintptr_t)PG_UINT32_MAX)
 #if SIZEOF_DATUM == 8
 #define SET_8_BYTES(value)	((Datum) (value))
 #elif SIZEOF_DATUM > 8
-#define SET_8_BYTES(value)	(((Datum) (value)) & 0xffffffffffffffffull)
+#define SET_8_BYTES(value)	(((Datum) (value)) & (uintptr_t)PG_UINT64_MAX)
 #endif
 
 /*
