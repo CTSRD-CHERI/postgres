2ab979d9022eb9f474601d1f96f96114b91b5f1c
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2016-10-27 18:52:04 +0000

    Add a build script for plain MIPS

--- build-for-cheribsd-mips.sh
+++ build-for-cheribsd-mips.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+set -e
 CHERISDK="/home/alr48/cheri/output/sdk256/bin"
 CHERIBSD_SYSROOT="/home/alr48/cheri/output/sdk256/sysroot"
 export PATH=${CHERISDK}:$PATH
 export CC=${CHERISDK}/clang
 export CXX=${CHERISDK}/clang++
 READLINE_INCLUDE_DIR=${CHERIBSD_SYSROOT}/usr/include/edit/
-COMMON_FLAGS="--sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -mabi=sandbox -msoft-float -mxgot"
+COMMON_FLAGS="--sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -mabi=64 -msoft-float"
 COMPILE_FLAGS="${COMMON_FLAGS} -isystem ${READLINE_INCLUDE_DIR} -Werror=cheri-capability-misuse -Werror=implicit-function-declaration -Werror=format -Werror=undefined-internal"
 export CFLAGS=${COMPILE_FLAGS}
 export CXXFLAGS=${COMPILE_FLAGS}
@@ -16,7 +17,9 @@ export LDFLAGS="${COMMON_FLAGS} -pthread"
 # env | sort
 # ./configure --host=cheri-unknown-freebsd --target=cheri-unknown-freebsd --build=x86_64-unknown-freebsd --prefix=/home/alr48/postgres --enable-debug
 # more minimal: --without-libxml --without-readline --without-gssapi
-./configure --host=cheri-unknown-freebsd --target=cheri-unknown-freebsd --build=x86_64-unknown-freebsd --prefix=/root/postgres-cheri/ --enable-debug --without-libxml --without-readline --without-gssapi
+./configure --host=mips64-unknown-freebsd --target=mips64-unknown-freebsd --build=x86_64-unknown-freebsd --prefix=/root/postgres-mips/ --enable-debug --without-libxml --without-readline --without-gssapi
 gmake -j16
-gmake install DESTDIR=/home/alr48/postgres-cheri
-gmake -C src/test/regress install-tests DESTDIR=/home/alr48/postgres-cheri
+gmake install DESTDIR=/home/alr48/postgres-mips
+gmake -C src/test/regress install-tests DESTDIR=/home/alr48/postgres-mips
+# won't work
+# gmake installcheck DESTDIR=/home/alr48/postgres-mips
