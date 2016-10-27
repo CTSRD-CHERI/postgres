ce3dc8b355f2b66421d8ba80a3335f41a4823443
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2016-10-27 18:48:28 +0000

    Add a script to run the regression test

---
+++ run-postgres-tests-cheri.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+# rm -rf /root/postgres-test-cheri
+mkdir -p $HOME/postgres-test-cheri/output/sql
+mkdir -p $HOME/postgres-test-cheri/output/expected
+mkdir -p $HOME/postgres-test-cheri/instance
+cd /root/postgres-test-cheri
+/root/postgres-cheri/lib/pgxs/src/test/regress/pg_regress --inputdir=/root/postgres-cheri/lib/regress/ --bindir=/root/postgres-cheri/bin --dlpath=/root/postgres-cheri/lib  --schedule=/root/postgres-cheri/lib/regress/serial_schedule --no-locale --outputdir=$HOME/postgres-test-cheri/output --temp-instance=$HOME/postgres-test-cheri/instance "$@"
