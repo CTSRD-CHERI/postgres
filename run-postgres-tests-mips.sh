aa464e8f6e483b3a3d2b014d9ffc70837f639de8
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2016-10-27 20:18:20 +0000

    Fix run scripts

--- run-postgres-tests-mips.sh
+++ run-postgres-tests-mips.sh
@@ -4,5 +4,4 @@ set -e
 mkdir -p $HOME/postgres-test-mips/output/sql
 mkdir -p $HOME/postgres-test-mips/output/expected
 mkdir -p $HOME/postgres-test-mips/instance
-cd /root/postgres-test-mips
 /root/postgres-mips/lib/pgxs/src/test/regress/pg_regress --inputdir=/root/postgres-mips/lib/regress/ --bindir=/root/postgres-mips/bin --dlpath=/root/postgres-mips/lib  --schedule=/root/postgres-mips/lib/regress/serial_schedule --no-locale --outputdir=$HOME/postgres-test-mips/output --temp-instance=$HOME/postgres-test-mips/instance "$@"
