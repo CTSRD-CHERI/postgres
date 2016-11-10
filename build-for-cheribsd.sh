c857582c5e45869fabe5f3d8ca8420b8acddf0e6
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2016-11-10 18:02:34 +0000

    allow building on a noexec fs and fix mips build

--- build-for-cheribsd.sh
+++ build-for-cheribsd.sh
@@ -6,7 +6,7 @@ export PATH=${CHERISDK}:$PATH
 export CC=${CHERISDK}/clang
 export CXX=${CHERISDK}/clang++
 READLINE_INCLUDE_DIR=${CHERIBSD_SYSROOT}/usr/include/edit/
-COMMON_FLAGS="--sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -target cheri-unknown-freebsd -mabi=sandbox -msoft-float -mxgot -O0 -DUSE_ASSERT_CHECKING -G0"
+COMMON_FLAGS="--sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -target cheri-unknown-freebsd -mabi=sandbox -msoft-float -mxgot -O0 -DUSE_ASSERT_CHECKING -G0 -integrated-as"
 COMPILE_FLAGS="${COMMON_FLAGS} -isystem ${READLINE_INCLUDE_DIR} -Werror=cheri-capability-misuse -Werror=implicit-function-declaration -Werror=format -Werror=undefined-internal"
 # export CFLAGS=${COMPILE_FLAGS}
 # export CXXFLAGS=${COMPILE_FLAGS}
@@ -16,7 +16,7 @@ COMPILE_FLAGS="${COMMON_FLAGS} -isystem ${READLINE_INCLUDE_DIR} -Werror=cheri-ca
 # LDFLAGS_SL  extra linker flags for linking shared libraries only
 # env | sort
 # more minimal: --without-libxml --without-readline --without-gssapi
-env PRINTF_SIZE_T_SUPPORT=yes "CFLAGS=${COMPILE_FLAGS}" "CXXFLAGS=${COMPILE_FLAGS}" "CPPFLAGS=${COMMON_FLAGS}" "LDFLAGS=${COMMON_FLAGS} -pthread" "LDFLAGS_EX=-static" ./configure --host=cheri-unknown-freebsd --target=cheri-unknown-freebsd --build=x86_64-unknown-freebsd --prefix=/postgres/cheri/ --enable-debug --without-libxml --without-readline --without-gssapi
+env PRINTF_SIZE_T_SUPPORT=yes "CFLAGS=${COMPILE_FLAGS}" "CXXFLAGS=${COMPILE_FLAGS}" "CPPFLAGS=${COMMON_FLAGS}" "LDFLAGS=${COMMON_FLAGS} -pthread" "LDFLAGS_EX=-static" sh ./configure --host=cheri-unknown-freebsd --target=cheri-unknown-freebsd --build=x86_64-unknown-freebsd --prefix=/postgres/cheri/ --enable-debug --without-libxml --without-readline --without-gssapi
 INSTALL_DIR=/exports/users/alr48
 gmake -j16
 gmake install DESTDIR=${INSTALL_DIR}
@@ -29,9 +29,9 @@ function do_objdump() {
     $CHERISDK/objdump -rlSd $1/$2 > $2.cheri.dump
 }
 
-do_objdump ./src/test/regress pg_regress
-do_objdump ./src/bin/initdb initdb
-do_objdump ./src/backend postgres
+#do_objdump ./src/test/regress pg_regress
+#do_objdump ./src/bin/initdb initdb
+#do_objdump ./src/backend postgres
 
 
 cp -fv run-postgres-tests-cheri.sh "${INSTALL_DIR}/postgres/run-postgres-tests-cheri.sh"
