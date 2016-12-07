ef8942dc46d6e058443e6d3aa4ab3b0cf55c5c82
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2016-12-07 14:02:23 +0000

    Add the new linker emulation

--- build-for-cheribsd-from-linux.sh
+++ build-for-cheribsd-from-linux.sh
@@ -16,7 +16,7 @@ COMPILE_FLAGS="${COMMON_FLAGS} -isystem ${READLINE_INCLUDE_DIR} -Werror=cheri-ca
 # LDFLAGS_SL  extra linker flags for linking shared libraries only
 # env | sort
 # more minimal: --without-libxml --without-readline --without-gssapi
-env PRINTF_SIZE_T_SUPPORT=yes "ZIC=/usr/sbin/zic" "CFLAGS=${COMPILE_FLAGS}" "CXXFLAGS=${COMPILE_FLAGS}" "CPPFLAGS=${COMMON_FLAGS}" "LDFLAGS=${COMMON_FLAGS} -pthread -static -fuse-ld=lld" "LDFLAGS_EX=-static" ./configure --host=cheri-unknown-freebsd --target=cheri-unknown-freebsd --build=x86_64-unknown-linux-gnu --prefix=/postgres/cheri/ --enable-debug --without-libxml --without-readline --without-gssapi
+env PRINTF_SIZE_T_SUPPORT=yes "ZIC=/usr/sbin/zic" "CFLAGS=${COMPILE_FLAGS}" "CXXFLAGS=${COMPILE_FLAGS}" "CPPFLAGS=${COMMON_FLAGS}" "LDFLAGS=${COMMON_FLAGS} -pthread -static -fuse-ld=lld -Wl,-melf64btsmip_cheri_fbsd" "LDFLAGS_EX=-static" ./configure --host=cheri-unknown-freebsd --target=cheri-unknown-freebsd --build=x86_64-unknown-linux-gnu --prefix=/postgres/cheri/ --enable-debug --without-libxml --without-readline --without-gssapi
 INSTALL_DIR=${PSOTGRES_INSTALL_DIR:-/build/ctsrd/postgres-install}
 gmake -j8
 gmake install DESTDIR=${INSTALL_DIR}
