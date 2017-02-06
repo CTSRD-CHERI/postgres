2adb0f75051ab90bcee5753130075af6d4f9d673
Edward Tomasz Napierala <trasz@FreeBSD.org>
2017-02-06 13:04:25 +0000

    Fix option names.

--- run-initdb-cheri.sh
+++ run-initdb-cheri.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rm -rf /home/postgres/postgres-test-cheri/instance/data
-"/postgres/cheri/bin/initdb" -D "/home/postgres/postgres-test-cheri/instance/data" --no-clean --no-sync --no-locale "$@"
+"/postgres/cheri/bin/initdb" -D "/home/postgres/postgres-test-cheri/instance/data" --noclean --nosync --no-locale "$@"
