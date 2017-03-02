b7ee5384bc743761a4da9e502ea191574892f82a
Edward Napierala <en322@thyme.cl.cam.ac.uk>
2017-03-02 11:31:35 +0000

    Cleanup, and use -j8 by default.

--- build-for-cheribsd.sh
+++ build-for-cheribsd.sh
@@ -1,28 +1,19 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/sh -e
 CHERI_ROOT="${HOME}/cheri"
 CHERISDK="${CHERI_ROOT}/output/sdk256/bin"
 CHERIBSD_SYSROOT="${CHERI_ROOT}/output/sdk256/sysroot"
+READLINE_INCLUDE_DIR=${CHERIBSD_SYSROOT}/usr/include/edit/
+INSTALL_DIR=${CHERI_ROOT}/output/rootfs256
+
 export PATH=${CHERISDK}:${CHERILDDIR}:$PATH
 export CC=${CHERISDK}/clang
 export CXX=${CHERISDK}/clang++
-READLINE_INCLUDE_DIR=${CHERIBSD_SYSROOT}/usr/include/edit/
 COMMON_FLAGS="-pipe --sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -target cheri-unknown-freebsd -mabi=sandbox -msoft-float -mxgot -O0 -static -DUSE_ASSERT_CHECKING  -G0 -integrated-as"
 COMPILE_FLAGS="${COMMON_FLAGS} -isystem ${READLINE_INCLUDE_DIR} -Werror=cheri-capability-misuse -Werror=implicit-function-declaration -Werror=format -Werror=undefined-internal -Werror=incompatible-pointer-types"
-# export CFLAGS=${COMPILE_FLAGS}
-# export CXXFLAGS=${COMPILE_FLAGS}
-# export CPPFLAGS=${COMMON_FLAGS}
-# export LDFLAGS="${COMMON_FLAGS} -pthread"
-# LDFLAGS_EX  extra linker flags for linking executables only
-# LDFLAGS_SL  extra linker flags for linking shared libraries only
-# env | sort
-# more minimal: --without-libxml --without-readline --without-gssapi
-# TODO: static? "LDFLAGS_EX=-static"
+
 env PRINTF_SIZE_T_SUPPORT=yes "CFLAGS=${COMPILE_FLAGS}" "CXXFLAGS=${COMPILE_FLAGS}" "CPPFLAGS=${COMMON_FLAGS}" "LDFLAGS=${COMMON_FLAGS} -pthread -Wl,-melf64btsmip_cheri_fbsd" sh ./configure --host=cheri-unknown-freebsd --target=cheri-unknown-freebsd --build=x86_64-unknown-freebsd --prefix=/postgres/cheri/ --enable-debug --without-libxml --without-readline --without-gssapi
 #INSTALL_DIR=/exports/users/alr48
-INSTALL_DIR=${CHERI_ROOT}/output/rootfs256
-#gmake -j16
-gmake
+gmake -j8
 gmake install DESTDIR=${INSTALL_DIR}
 gmake -C src/test/regress install-tests DESTDIR=${INSTALL_DIR}
 
