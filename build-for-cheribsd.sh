e08e2cda445589e194fa2061cc9e3e00b287656c
Edward Napierala <en322@cl.cam.ac.uk>
2019-12-16 15:28:12 +0000

    Install into /postgres, not /postgres/cheri/.

--- build-for-cheribsd.sh
+++ build-for-cheribsd.sh
@@ -26,14 +26,14 @@ COMMON_FLAGS="-pipe --sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -target cheri-un
 COMPILE_FLAGS="${COMMON_FLAGS} -isystem ${READLINE_INCLUDE_DIR} -Werror=cheri-capability-misuse -Werror=implicit-function-declaration -Werror=format -Werror=undefined-internal -Werror=incompatible-pointer-types"
 
 #env PRINTF_SIZE_T_SUPPORT=yes "CFLAGS=${COMPILE_FLAGS}" "CXXFLAGS=${COMPILE_FLAGS}" "CPPFLAGS=${COMMON_FLAGS}" "LDFLAGS=${COMMON_FLAGS} ${CFLAGS_LIBSTATCOUNTERS} -fuse-ld=lld -pthread -Wl,-melf64btsmip_cheri_fbsd" sh ./configure --host=cheri-unknown-freebsd --target=cheri-unknown-freebsd --build=x86_64-unknown-freebsd --prefix=/postgres/cheri/ --without-libxml --without-readline --without-gssapi
-env PRINTF_SIZE_T_SUPPORT=yes "CFLAGS=${COMPILE_FLAGS}" "CXXFLAGS=${COMPILE_FLAGS}" "CPPFLAGS=${COMMON_FLAGS}" "LDFLAGS=${COMMON_FLAGS} ${CFLAGS_LIBSTATCOUNTERS} -pthread" sh ./configure --host=cheri-unknown-freebsd --target=cheri-unknown-freebsd --build=x86_64-unknown-freebsd --prefix=/postgres/cheri/ --without-libxml --without-readline --without-gssapi
+env PRINTF_SIZE_T_SUPPORT=yes "CFLAGS=${COMPILE_FLAGS}" "CXXFLAGS=${COMPILE_FLAGS}" "CPPFLAGS=${COMMON_FLAGS}" "LDFLAGS=${COMMON_FLAGS} ${CFLAGS_LIBSTATCOUNTERS} -pthread" sh ./configure --host=cheri-unknown-freebsd --target=cheri-unknown-freebsd --build=x86_64-unknown-freebsd --prefix=/postgres/ --without-libxml --without-readline --without-gssapi
 #INSTALL_DIR=/exports/users/alr48
 gmake -j8 clean 
 gmake -j8 all
 gmake install DESTDIR=${INSTALL_DIR}
 gmake -C src/test/regress install-tests DESTDIR=${INSTALL_DIR}
 
-cp run-postgres-tests.sh "${INSTALL_DIR}/postgres/cheri/run-postgres-tests.sh"
+cp run-postgres-tests.sh "${INSTALL_DIR}/postgres/run-postgres-tests.sh"
 #cp run-initdb-cheri.sh "${INSTALL_DIR}/postgres/run-initdb-cheri.sh"
 #cp postgres-benchmark.sh "${INSTALL_DIR}/postgres-benchmark.sh"
 #chmod 755 "${INSTALL_DIR}/postgres-benchmark.sh"
